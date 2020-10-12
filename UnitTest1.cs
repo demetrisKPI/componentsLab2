e9900b6e93627af1f0e7efa5c9f38b12c0edd7f9
demetrisKPI <muzhnat@ukr.net>
2020-10-12 18:49:29 +0300

    restruct-tests

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,37 +5,32 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
-        
         private static string _newsalt = "here goes the salt";
         private static string _password = "here goes the password";
         private static string _password2 = "this is another password";
         private static uint _modAdler32 = 65521;
+        [Fact]
         private void TestGetHash()
         {
             Assert.NotNull(PasswordHasher.GetHash(_password));
         }
+        [Fact]
         private void TestPasswordSaltRelation()
         {
             Assert.Equal(PasswordHasher.GetHash(_password), PasswordHasher.GetHash(_password));
         }
+        [Fact]
         private void TestHashDifference()
         {
             Assert.NotEqual(PasswordHasher.GetHash(_password), PasswordHasher.GetHash(_password2));
         }
+        [Fact]
         private void TestNewSaltWithInit()
         {
             string withOldSalt = PasswordHasher.GetHash(_password);
             PasswordHasher.Init(_newsalt, _modAdler32);
             string withNewSalt = PasswordHasher.GetHash(_password);
             Assert.NotEqual(withOldSalt, withNewSalt);
-        }
-        [Fact]
-        public void Test()
-        {
-            TestGetHash();
-            TestPasswordSaltRelation();
-            TestHashDifference();
-            TestNewSaltWithInit();
         }
     }
 }
